4b7d3045306a1155b78e245686e5d5821d58d93e
nandinisaka <168847945+nandinisaka@users.noreply.github.com>
2024-07-09 16:26:10 +0530

    Delete Back end directory
    Delete

--- Back end/java/com/art/gallery/repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.art.galley.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.art.galley.entity.Contact;
-
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-
-}
